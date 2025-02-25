14c0f824e957288799948e8c471369a85a128541
Thomas B. <info@smsmobileapi.com>
2025-02-25 23:21:29 +0100

    Update SMSMobileAPITest.php

--- test/SMSMobileAPITest.php
+++ test/SMSMobileAPITest.php
@@ -1,11 +1,16 @@
-<?php
+<?php 
 
-require 'vendor/autoload.php';
+$apiKey = 'YOUR-API-KEY';
+$recipients = '+123456789';
+$message = 'Hello from SMS Mobile API PHP SDK!';
+
+// Send a scheduled message via WhatsApp and SMS.
+//$scheduleTimestamp = strtotime('+1 hour'); //Scheduled in 1 hour (GMT 0).
+$scheduleTimestamp = "";
+
+require_once __DIR__ . '/smsmobileapi-php/vendor/autoload.php';
 
 use SMSMobileAPI\SMSMobileAPI;
-
-$apiKey = 'YOUR_API_KEY';
-
 $smsAPI = new SMSMobileAPI($apiKey);
 
 // Vérifier si la classe et la méthode existent
@@ -16,19 +21,11 @@ if (!method_exists($smsAPI, 'sendMessage')) {
     die('❌ Method sendMessage() not found in SMSMobileAPI class!');
 }
 
-// Définition des paramètres du message
-$recipients = '+123456789';
-$message = 'Hello from SMS Mobile API PHP SDK!';
-// Send a scheduled message via WhatsApp and SMS.
-//$scheduleTimestamp = strtotime('+1 hour'); //Scheduled in 1 hour (GMT 0).
-$scheduleTimestamp = "";
-
-// Envoi du message
 $response = $smsAPI->sendMessage(
-    $recipients,
-    $message,
-    true,  // Envoyer via WhatsApp
-    true,  // Envoyer aussi en SMS
+    $recipients, 
+    $message, 
+    false,  // Send via WhatsApp
+    true, // Send aussi en SMS
     $scheduleTimestamp
 );
 
@@ -36,3 +33,7 @@ $response = $smsAPI->sendMessage(
 echo "<pre>";
 print_r($response);
 echo "</pre>";
+
+?>
+
+
